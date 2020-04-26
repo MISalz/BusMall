c181e3fda925e8d79deeb18e8ecca2808ad910aa
Michelle Salazar <michellesalazar010@gmail.com>
2020-04-26 01:08:46 -0700

    update js

--- js/app.js
+++ js/app.js
@@ -2,83 +2,84 @@
 
 console.log('its Saturday!');
 
-var pIndex = [];
+var busMall = [];
 
-// link to dom
+// link to html image place holders
 var choiceOne = document.getElementById('first_choice');
 var choiceTwo = document.getElementById('second_choice');
 var choiceThree = document.getElementById('third_choice');
 var divElement = document.getElementById('product-options');
 
+//parent element to write to dom
+var pElement = document.getElementById('tally');
 //track number of clicks
 var tracker = 25;
 
 // Create a constructor
-function Shelf(opt, src){
-  this.opt = opt;
+function PictChoices(name, src){
+  this.name = name;
   this.src = src;
   this.clicks = 0;
-
-  pIndex.push(this);
+  busMall.push(this);
 }
 // Create an algorithm that will randomly generate three unique product images
-function randomizer(max) {
+function randomizer(max){
   return Math.floor(Math.random() * max);
 }
 
-function imgChoices() {
-  var pic1 = randomizer(pIndex.length);
+function imgChoices(){
+  var pic1 = randomizer(busMall.length);
   console.log(pic1);
-  var pic2 = randomizer(pIndex.length);
+  var pic2 = randomizer(busMall.length);
   console.log(pic2);
-  var pic3 = randomizer(pIndex.length);
+  var pic3 = randomizer(busMall.length);
   console.log(pic3);
 
-  choiceOne.src = pIndex[pic1].src;
-  choiceOne.title = pIndex[pic1].opt;
+  choiceOne.src = busMall[pic1].src;
+  choiceOne.title = busMall[pic1].name;
 
-  choiceTwo.src = pIndex[pic2].src;
-  choiceTwo.title = pIndex[pic2].opt;
+  choiceTwo.src = busMall[pic2].src;
+  choiceTwo.title = busMall[pic2].name;
 
-  choiceThree.src = pIndex[pic3].src;
-  choiceThree.title = pIndex[pic3].opt;
+  choiceThree.src = busMall[pic3].src;
+  choiceThree.title = busMall[pic3].name;
 }
 
 function endChoices() {
   divElement.removeEventListener('click', handleClick);
-  divElement.textContent ='';
+  divElement.textContent = '';
   alert ('Thank you for taking the time to complete the survey.');
 }
 
-new Shelf('bag', './images/bag.jpg');
-new Shelf('banana', './images/banana.jpg');
-new Shelf('bathroom', './images/bathroom.jpg');
-new Shelf('boots', './images/boots.jpg');
-new Shelf('breakfast', './images/breakfast.jpg');
-new Shelf('bubblegum', './images/bubblegum.jpg');
-new Shelf('chair', './images/chair.jpg');
-new Shelf('cthulhu', './images/cthulhu.jpg');
-new Shelf('DD', './images/dog-duck.jpg');
-new Shelf('dragon', './images/dragon.jpg');
-new Shelf('pen', './images/pen.jpg');
-new Shelf('sweep', './images/pet-sweep.jpg');
-new Shelf('scissors', './images/scissors.jpg');
-new Shelf('shark', './images/shark.jpg');
-new Shelf('sweep', './images/sweep.png');
-new Shelf('tauntaun', './images/tauntaun.jpg');
-new Shelf('unicorn', './images/unicorn.jpg');
-new Shelf('usb', './images/usb.gif');
-new Shelf('water', './images/water-can.jpg');
-new Shelf('wine', './images/wine-glass.jpg');
+new PictChoices('bag', './images/bag.jpg');
+new PictChoices('banana', './images/banana.jpg');
+new PictChoices('bathroom', './images/bathroom.jpg');
+new PictChoices('boots', './images/boots.jpg');
+new PictChoices('breakfast', './images/breakfast.jpg');
+new PictChoices('bubblegum', './images/bubblegum.jpg');
+new PictChoices('chair', './images/chair.jpg');
+new PictChoices('cthulhu', './images/cthulhu.jpg');
+new PictChoices('DD', './images/dog-duck.jpg');
+new PictChoices('dragon', './images/dragon.jpg');
+new PictChoices('pen', './images/pen.jpg');
+new PictChoices('sweep', './images/pet-sweep.jpg');
+new PictChoices('scissors', './images/scissors.jpg');
+new PictChoices('shark', './images/shark.jpg');
+new PictChoices('sweep', './images/sweep.png');
+new PictChoices('tauntaun', './images/tauntaun.jpg');
+new PictChoices('unicorn', './images/unicorn.jpg');
+new PictChoices('usb', './images/usb.gif');
+new PictChoices('water', './images/water-can.jpg');
+new PictChoices('wine', './images/wine-glass.jpg');
 
 
-
-//Once the users ‘clicks’ a product, generate three new products for the user to pick from.
+//Once the users ‘clicks’ a product, generate three new products for the user to pick from and track # of clicks
 function handleClick(event){
   var productChoice = event.target.title;
-  for(var i = 0; i < pIndex.length; i++){
-    if (productChoice === pIndex[i].name){
-      pIndex[i].clicks++;
+  console.log(productChoice);
+  for(var i = 0; i < busMall.length; i++){
+    if (productChoice === busMall[i].name){
+      busMall[i].clicks++;
     }
   }
   tracker --;
@@ -93,3 +94,9 @@ divElement.addEventListener('click', handleClick);
 
 imgChoices();
 
+//write to nav bar total clicks
+for(var i = 0 ; i < busMall.length; i++){
+  var imgEl = document.createElement('li');
+  imgEl.textContent = busMall[i];
+  pElement.appendChild(imgEl);
+}
