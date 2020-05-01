ba3f2834bf3dfa596acb75535b66bbd8a892fd20
Michelle Salazar <michellesalazar010@gmail.com>
2020-04-30 20:54:28 -0700

    add chart

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 'use usestrict';
 
 var busMall = [];
-console.log(busMall);
 
 // link to html image place holders
 var choiceOne = document.getElementById('first_choice');
@@ -9,16 +8,17 @@ var choiceTwo = document.getElementById('second_choice');
 var choiceThree = document.getElementById('third_choice');
 var divElement = document.getElementById('product-options');
 
-//parent element to write to dom
+// //parent element to write to dom
 var pElement = document.getElementById('tally');
 //track number of clicks
-var tracker = 25;
+var tracker = 5;
 
 // Create a constructor
 function PictChoices(name, src){
   this.name = name;
   this.src = src;
   this.clicks = 0;
+  this.turns = 0;
   busMall.push(this);
 }
 // Create an algorithm that will randomly generate three unique product images
@@ -28,11 +28,9 @@ function randomizer(max){
 
 function imgChoices(){
   var pic1 = randomizer(busMall.length);
-  console.log(pic1);
   var pic2 = randomizer(busMall.length);
-  console.log(pic2);
   var pic3 = randomizer(busMall.length);
-  console.log(pic3);
+
 
   choiceOne.src = busMall[pic1].src;
   choiceOne.title = busMall[pic1].name;
@@ -42,56 +40,137 @@ function imgChoices(){
 
   choiceThree.src = busMall[pic3].src;
   choiceThree.title = busMall[pic3].name;
+
+  busMall[pic1].turns++;
+  busMall[pic2].turns++;
+  busMall[pic3].turns++;
+
 }
 
 function endChoices() {
   divElement.removeEventListener('click', handleClick);
   divElement.textContent = '';
-  alert ('Thank you for taking the time to complete the survey.');
+  console.log('Thank you for taking the time to complete the survey.');
 }
 
-new PictChoices('bag', './images/bag.jpg');
-new PictChoices('banana', './images/banana.jpg');
-new PictChoices('bathroom', './images/bathroom.jpg');
-new PictChoices('boots', './images/boots.jpg');
-new PictChoices('breakfast', './images/breakfast.jpg');
-new PictChoices('bubblegum', './images/bubblegum.jpg');
-new PictChoices('chair', './images/chair.jpg');
-new PictChoices('cthulhu', './images/cthulhu.jpg');
-new PictChoices('DD', './images/dog-duck.jpg');
-new PictChoices('dragon', './images/dragon.jpg');
-new PictChoices('pen', './images/pen.jpg');
-new PictChoices('sweep', './images/pet-sweep.jpg');
-new PictChoices('scissors', './images/scissors.jpg');
-new PictChoices('shark', './images/shark.jpg');
-new PictChoices('sweep', './images/sweep.png');
-new PictChoices('tauntaun', './images/tauntaun.jpg');
-new PictChoices('unicorn', './images/unicorn.jpg');
-new PictChoices('usb', './images/usb.gif');
-new PictChoices('water', './images/water-can.jpg');
-new PictChoices('wine', './images/wine-glass.jpg');
+
+function seedChartData(){
+  var clicksData = [];
+  var labelData = [];
+  var viewsData = [];
+
+  for (var i =  0; i < busMall.length; i++){
+    clicksData.push (busMall[i].clicks);
+    labelData.push (busMall[i].name);
+    viewsData.push (busMall[i].turns);
+  }
+  return [clicksData, labelData, viewsData];
+}
+
+function renderChart() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: seedChartData()[1],
+      datasets: [{
+        label: '# of clicks',
+        data: seedChartData()[0],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: '# of Views',
+        data:  seedChartData()[2],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+  console.log('here');
+}
+
+new PictChoices('Bag', './images/bag.jpg');
+new PictChoices('Banana', './images/banana.jpg');
+new PictChoices('Bathroom', './images/bathroom.jpg');
+new PictChoices('Boots', './images/boots.jpg');
+new PictChoices('Breakfast', './images/breakfast.jpg');
+new PictChoices('Bubblegum', './images/bubblegum.jpg');
+new PictChoices('Chair', './images/chair.jpg');
+new PictChoices('Cthulhu', './images/cthulhu.jpg');
+new PictChoices('DogDuck', './images/dog-duck.jpg');
+new PictChoices('Dragon', './images/dragon.jpg');
+new PictChoices('Pen', './images/pen.jpg');
+new PictChoices('PetSweep', './images/pet-sweep.jpg');
+new PictChoices('Scissors', './images/scissors.jpg');
+new PictChoices('Shark', './images/shark.jpg');
+new PictChoices('Sweep', './images/sweep.png');
+new PictChoices('Tauntaun', './images/tauntaun.jpg');
+new PictChoices('Unicorn', './images/unicorn.jpg');
+new PictChoices('Usb', './images/usb.gif');
+new PictChoices('Water', './images/water-can.jpg');
+new PictChoices('Wine', './images/wine-glass.jpg');
 
 
 //Once the users ‘clicks’ a product, generate three new products for the user to pick from and track # of clicks
 function handleClick(event){
   var productChoice = event.target.title;
-  console.log(productChoice);
   for(var i = 0; i < busMall.length; i++){
     if (productChoice === busMall[i].name){
       busMall[i].clicks++;
-      console.log(busMall[i]);
     }
   }
   tracker --;
+
   if (tracker === 0 ){
     endChoices();
+    renderChart();
     //write to nav bar total clicks
-    for(var i = 0 ; i < busMall.length; i++){
+    for(var j = 0 ; j < busMall.length; j++){
       var imgEl = document.createElement('li');
-      imgEl.textContent = busMall[i].name;
+      imgEl.textContent = busMall[j].name;
 
       var clEL = document.createElement('li');
-      clEL.textContent = busMall[i].clicks;
+      clEL.textContent = busMall[j].clicks;
       pElement.appendChild(imgEl);
       pElement.appendChild(clEL);
     }
@@ -101,5 +180,4 @@ function handleClick(event){
 divElement.addEventListener('click', handleClick);
 
 imgChoices();
-
 
